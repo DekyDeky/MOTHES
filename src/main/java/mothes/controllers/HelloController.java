a403c9bfe01bbb9def101679ce1c5cd6ee6d6aac
Ricardo Hideki Tsugikava Junior <166323985+DekyDeky@users.noreply.github.com>
2025-11-14 19:09:54 -0300

    feat: transparent

--- src/main/java/mothes/controllers/HelloController.java
+++ src/main/java/mothes/controllers/HelloController.java
@@ -9,7 +9,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -24,21 +26,45 @@ public class HelloController {
     private Scene scene;
     private Parent root;
 
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     public void login(ActionEvent event) throws IOException {
 
         String email = emailField.getText();
         String senha = passwordField.getText();
 
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/mothes/home.fxml"));
         root = loader.load();
+
+        root.setOnMousePressed(Mevent -> {
+            xOffset = Mevent.getSceneX();
+            yOffset = Mevent.getSceneY();
+        });
+
+        root.setOnMouseDragged(Mevent -> {
+            stage.setX(Mevent.getScreenX() - xOffset);
+            stage.setY(Mevent.getScreenY() - yOffset);
+        });
 
         HomeController homePage = loader.getController();
         homePage.displayTeste(email, senha);
 
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = new Stage();
         scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+
+        String style = this.getClass().getResource("/mothes/styles/home.css").toExternalForm();
+        scene.getStylesheets().add(style);
+
+        stage.initStyle(StageStyle.TRANSPARENT);
+
         stage.setScene(scene);
         stage.show();
+
+        Stage stageAnterior = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stageAnterior.close();
 
     }
 
